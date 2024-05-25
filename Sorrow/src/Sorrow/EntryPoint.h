03fdb3acedcebb0bba29b6367d304f1e152b3ddd
Glockie-Bae <2358750802@qq.com>
2024-05-25 14:54:53 +0800

    Premake

--- Sorrow/src/Sorrow/EntryPoint.h
+++ Sorrow/src/Sorrow/EntryPoint.h
@@ -8,7 +8,7 @@ extern Sorrow::Application* Sorrow::CreateApplication();
 int main(int argc, char** argv) {
 	Sorrow::Log::Init();
 	SW_CORE_INFO("Initialized Log");
-	SW_Client_ERROR("Hello");
+	SW_Client_TRACE("Hello");
 
 	std::cout << "Start Engine!" << std::endl;
 	auto app = Sorrow::CreateApplication();
---
+++ premake5.lua
@@ -0,0 +1,101 @@
+workspace "Sorrow"
+	architecture"x64"
+
+	configurations{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Sorrow"
+	location "Sorrow"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+	
+
+	defines{
+	"SORROW_PLATFORM_WINDOWS",
+	"SORROW_BUILD_DLL"
+
+	}
+
+	postbuildcommands{
+		("{COPYDIR} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+	}
+
+	filter"configurations:Debug"
+	    defines "SW_DEBUG"
+	    symbols "On"
+
+    filter"configurations:Release"
+	    defines "SW_RELEASE"
+	    optimize "On"
+
+	filter"configurations:Dist"
+	    defines "SW_DIST"
+	    optimize "On"
+
+
+project "Sandbox"
+    location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs{
+		"Sorrow/vendor/spdlog/include",
+		"Sorrow/src"
+	}
+
+	links{
+		"Sorrow"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+	
+
+	    defines{
+	    "SORROW_PLATFORM_WINDOWS",
+	    }
+
+
+	filter"configurations:Debug"
+	    defines "SW_DEBUG"
+	    symbols "On"
+
+    filter"configurations:Release"
+	    defines "SW_RELEASE"
+	    optimize "On"
+
+	filter"configurations:Dist"
+	    defines "SW_DIST"
+	    optimize "On"
